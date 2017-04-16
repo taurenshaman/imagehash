114df646adf69426ad7635cf050ea30f1cc5800d
Jerin Lee <taurenjerin@qq.com>
2017-04-16 09:12:42 +0800

    add a C# solution; add some images.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

---
+++ csharp/ConsoleTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace ConsoleTest {
+  class Program {
+    static void Main(string[] args) {
+      testImageHashUsingCommand();
+
+
+      Console.WriteLine( "done." );
+      Console.ReadKey();
+    }
+    
+    // failed: IronPython cannot process .pyd files in PIL
+    static void testImageHashUsingIronPython() {
+      string dir = @"E:\Github\imagehash\images\";
+      string file1 = dir + "imagehash.png";
+      string file2 = dir + "lenna.png";
+      string file3 = dir + "lenna1.jpg";
+      string file4 = dir + "lenna2.jpg";
+
+      Console.WriteLine( file1 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash1 = Vision.Runtime.Python.ComputeImageWHash( file1 );
+      Console.WriteLine( hash1 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file2 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash2 = Vision.Runtime.Python.ComputeImageWHash( file2 );
+      Console.WriteLine( hash2 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file3 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash3 = Vision.Runtime.Python.ComputeImageWHash( file3 );
+      Console.WriteLine( hash3 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file4 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash4 = Vision.Runtime.Python.ComputeImageWHash( file4 );
+      Console.WriteLine( hash4 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+    }
+
+    // worked @ 2017-4-15
+    static void testImageHashUsingCommand() {
+      string dir = @"E:\Github\imagehash\images\";
+      string file1 = dir + "imagehash.png";
+      string file2 = dir + "lenna.png";
+      string file3 = dir + "lenna1.jpg";
+      string file4 = dir + "lenna2.jpg";
+
+      Console.WriteLine( file1 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash1 = Vision.Runtime.PythonCommond.ComputeImageWHash( file1 );
+      Console.WriteLine( hash1 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file2 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash2 = Vision.Runtime.PythonCommond.ComputeImageWHash( file2 );
+      Console.WriteLine( hash2 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file3 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash3 = Vision.Runtime.PythonCommond.ComputeImageWHash( file3 );
+      Console.WriteLine( hash3 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+
+      Console.WriteLine( file4 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+      string hash4 = Vision.Runtime.PythonCommond.ComputeImageWHash( file4 );
+      Console.WriteLine( hash4 );
+      Console.WriteLine( DateTime.Now.ToLongTimeString() );
+    }
+
+  }
+
+}
---
+++ csharp/Vision/Runtime/PythonCommond.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vision.Runtime {
+  public static class PythonCommond {
+    static string PythonExecutePath = @"D:\Python27\python.exe";
+
+
+    public static string Excute(int millisecondsWaitForExit, string pythonScriptPath, params string[] parameters) {
+      StringBuilder sbParams = new StringBuilder();
+      if(parameters != null) {
+        foreach (string param in parameters)
+          sbParams.Append( " " + param );
+      }
+
+      string output = "";
+
+      ProcessStartInfo startInfo = new ProcessStartInfo();
+      startInfo.FileName = PythonExecutePath;
+      startInfo.Arguments = string.Format( "{0} {1}", pythonScriptPath, sbParams.ToString() ); ;
+      startInfo.UseShellExecute = false;
+      startInfo.RedirectStandardInput = true;
+      startInfo.RedirectStandardOutput = true;
+      startInfo.CreateNoWindow = true;
+
+      using (Process process = new Process()) {
+        process.StartInfo = startInfo;
+        //process.OutputDataReceived += (s, e) => {
+        //  System.Diagnostics.Debug.WriteLine( e.Data );
+        //};
+        try {
+          if (process.Start())
+          {
+            if (millisecondsWaitForExit == 0)
+              process.WaitForExit();
+            else
+              process.WaitForExit( millisecondsWaitForExit );
+            output = process.StandardOutput.ReadToEnd();
+          }
+        }
+        catch (Exception ex) {
+          Debug.WriteLine( ex.Message );
+        }
+        finally {
+          if (process != null)
+            process.Close();
+        }
+      }
+
+      return output;
+    }
+
+    public static string ComputeImageWHash(string imagePath, int millisecondsWaitForExit = 0) {
+      string output = Excute( millisecondsWaitForExit, @"E:\Github\imagehash\python\computeimagehash.py", imagePath );
+      return output;
+    }
+
+  }
+
+}
